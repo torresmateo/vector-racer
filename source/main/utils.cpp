08aeb6823d9731a1204f16821ecc179c269b0ca1
Maluchi <torresmateo@gmail.com>
2013-01-10 04:09:25 -0300

    agregada la funcio?n torusSection

--- source/main/utils.cpp
+++ source/main/utils.cpp
@@ -85,6 +85,35 @@ string doubleToStr(double num) {
 }
 
 
+/************************************************************
+	funciones que determinan la forma de la carretera
+*************************************************************/
+
+PathSection torusSection(float innerRadius, float outerRadius, unsigned int rings, float sectionAngleDeg, float orientationAngleDeg){
+	//innerRadius: el radio de cada segmento
+	//outerRadius: el radio del torus
+	//rings: cuantos segmentos tiene esta sección
+	//sectionAngleDeg que tantos grados del toro abarca
+	//orientationAngleDeg hacia donde se curva el toro
+	
+	float segmentAngleDeg = sectionAngleDeg/rings; //ángulo abarcado por cada segmento
+	float x = outerRadius * cos(orientationAngleDeg * (PI/180));
+	float y = outerRadius * sin(orientationAngleDeg * (PI/180));
+	Vector3D radiusVector(-x,-y,0);//negativo para poder usar el origen como punto de rotación
+	Vector3D rotateAxis(0,0,1);
+	rotateAxis = Vector3D::crossMultiply(rotateAxis,radiusVector);
+	Vector3D rotatedRadius = rotateVertex(radiusVector,rotateAxis,segmentAngleDeg);
+	Vector3D tanVector = rotateVertex(Vector3D(0,0,1),rotateAxis,segmentAngleDeg);
+	PathSection torus;
+	torus.setRadius(innerRadius);
+	torus.setPositionEnd(radiusVector - rotatedRadius);
+	torus.setNormalEnd(tanVector);
+	torus.setNumberOfSegments(rings);
+	return torus;
+
+}
+
+
 void loadPath(){
 
 	Vector3D aux1(0,0,0);
@@ -100,35 +129,18 @@ void loadPath(){
 	Vector3D aux7(0,0,0.5f);
 	Vector3D aux8(0.1f,0.1f,1);
 	
-	PathSection seccion(	0.4, 
-		aux1, aux2,
-		aux3, aux4,
-		1
-	);
-	PathSection seccion2(0.4, 
-		aux5, aux6,
-		aux7, aux8,
-		1
-	);
+	int rings = 50;
+	float sectionAngle = 45.0f;
+	PathSection seccion (torusSection(0.5f, 10.0f,rings, sectionAngle, 0.0f));
+	PathSection seccion2(torusSection(0.5f, 10.0f,rings, sectionAngle, 180.0f));
+	PathSection seccion3(torusSection(0.5f, 10.0f,rings, sectionAngle, 45.0f));
+	PathSection seccion4(torusSection(0.5f, 10.0f,rings, sectionAngle, -45.0f));
+	
 
 	gTUNNEL_PATH.pushSection(seccion);
-	gTUNNEL_PATH.pushSection(seccion);
-	gTUNNEL_PATH.pushSection(seccion);
-	gTUNNEL_PATH.pushSection(seccion);
 	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion2);
-	/*gTUNNEL_PATH.pushSection(seccion2);
-	gTUNNEL_PATH.pushSection(seccion2);*/
+	gTUNNEL_PATH.pushSection(seccion3);
+	gTUNNEL_PATH.pushSection(seccion4);
 }
 
 
