8478ec53035f6d77d557a242b7c3c901b05b2b0d
Maluchi <Maluchi@Maluchi-DeskPC>
2013-01-13 14:22:30 -0300

    Arreglados Bugs del Loader, no entiendo porque no anda la iluminacion

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -5,11 +5,67 @@ int main(int argc, char **argv) {
 	
 	// init GLUT and create window
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+	//glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(550,150);
 	glutInitWindowSize(800,600);
 	glutCreateWindow("Titulo de la ventana");
 	
+
+
+
+
+/*
+	
+	  GLfloat light_ambient[] = { 0.75, 0.75, 0.75, 1.0 };
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_position[] = { 0.0, 0.0, 1.0, 0.0 };
+
+    glLightfv (GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv (GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv (GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv (GL_LIGHT0, GL_POSITION, light_position);
+    
+    glEnable (GL_LIGHTING);
+    glEnable (GL_LIGHT0);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_DEPTH_TEST);
+	
+*/
+	// OpenGL init
+	//glShadeModel(GL_SMOOTH);
+	//glEnable(GL_CULL_FACE);
+	//glEnable(GL_DEPTH_TEST);
+	
+	glEnable(GL_DEPTH_TEST);
+	
+	//system("pwd");
+	
+	
+	glEnable(GL_LIGHTING);
+    //glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHT0);
+    float col[]={1.0,1.0,1.0,1.0};
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,col);
+	glLightfv(GL_LIGHT0,GL_AMBIENT,col);
+	glLightfv(GL_LIGHT0,GL_SPECULAR,col);
+	
+	cube=obj.load("cardassiangalor.obj");	//load the test.obj file
+	//cube=obj.load("beetle.obj");	//load the test.obj file
+	//scube=obj.load("test.obj");	//load the test.obj file
+	
+	loadPath();
+	
+
+
+
+
+
+
+
+
+
 	// register callbacks
 	glutDisplayFunc(display);
 	glutReshapeFunc(changeSize);
@@ -27,27 +83,7 @@ int main(int argc, char **argv) {
 	glutMotionFunc(mouseActiveMove);
 	
 	
-	// OpenGL init
-	//glShadeModel(GL_SMOOTH);
-	//glEnable(GL_CULL_FACE);
-	//glEnable(GL_DEPTH_TEST);
-	
-	glEnable(GL_DEPTH_TEST);
-	
-	//system("pwd");
-	
-	
-	//glEnable(GL_LIGHTING);
-    //glEnable(GL_LIGHT0);
-    float col[]={1.0,1.0,1.0,1.0};
-    glLightfv(GL_LIGHT0,GL_DIFFUSE,col);
-	
-	
-	//cube=obj.load("cardassiangalor.obj");	//load the test.obj file
-	cube=obj.load("beetle.obj");	//load the test.obj file
-	
-	loadPath();
-	
+
 	// enter GLUT event processing cycle
 	glutMainLoop();
 	
