1a35de13fb8966a4fe9c69e809e70302473aab24
Maluchi <torresmateo@gmail.com>
2013-01-28 11:42:23 -0300

    Comentarios de Keyboard

--- source/input/keyboard.cpp
+++ source/input/keyboard.cpp
@@ -43,61 +43,69 @@ string Keyboard::asciiToString(){
 	return ss.str();
 }
 
-
+//retorna true si se esta presionando alguna tecla
 bool Keyboard::keyPressed(){
 	return asciiKeyPressed() or specialKeyPressed();
 }
 
-
+//retorna true si se esta presionando una tecla ASCII
 bool Keyboard::asciiKeyPressed(){
 	return !asciiKeys.empty();
 }
+
+//retorna true si la tecla ASCII "key" esta presionada
 bool Keyboard::asciiKeyPressed( unsigned char key ){
 	return asciiKeys.count( key )? true : false;
 }
 
-
+//retorna true si se esta presionando alguna tecla compuesta
 bool Keyboard::specialKeyPressed(){
 	return !specialKeys.empty();
 }
+
+//retorna true si la tecla compuesta "key" esta presionada
 bool Keyboard::specialKeyPressed( int key ){
 	return specialKeys.count( key )? true : false;
 }
 
-
+//inserta un caracer ASCII
 void Keyboard::insertAsciiKey( unsigned char key ){
 	asciiKeys.insert(key);
 }
 
+//inserta un caracter compuesto
 void Keyboard::insertSpecialKey( int key ){
 	specialKeys.insert(key);
 }
 
-
+//remueve un caracter ASCII
 void Keyboard::removeAsciiKey( unsigned char key ){
 	asciiKeys.erase(key);
 }
 
+//remueve un caracter compuesto
 void Keyboard::removeSpecialKey( int key ){
 	specialKeys.erase(key);
 }
 
-
+//retorna un set con los caracteres ASCII presionados
 set<unsigned char> Keyboard::getAsciiKeyPresses(){
 	return this->asciiKeys;
 }
 
+//retorna un set con los caracteres compuestos presionados
 set<int> Keyboard::getSpecialKeyPresses(){
 	return this->specialKeys;
 }
 
-
+//retorna el siguiente caracter ASCII del set
 unsigned char Keyboard::getNextAsciiKey(){
 	unsigned char key = *(asciiKeys.begin());
 	this->removeAsciiKey(key);
 	return key;
 }
 
+//retorna el siguiente caracter compuesto del set
 int Keyboard::getNextSpecialKey(){
 	int key = *(specialKeys.begin());
 	this->removeSpecialKey(key);
