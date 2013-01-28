55c654f2126a28f5c4e2e7205516536dcea0f59d
Maluchi <torresmateo@gmail.com>
2013-01-28 10:05:41 -0300

    terminados los comentarios de draw,(a excepcio?n de obstacle.hpp)

--- source/draw/pathSection.hpp
+++ source/draw/pathSection.hpp
@@ -1,24 +1,37 @@
 #ifndef PATH_SECTION_H
 #define PATH_SECTION_H
+
+//clase que reprsenta una sección del túnel
 class PathSection {
 	
+    //radio de la sección
 	float radius;
+    //número de segmentos que componen la sección
 	int numberOfSegments;
 	
+    //posición inicial del centro de la sección
 	Vector3D positionIni;
+    //normal inicial, perpendicular a un corte transversal al inicio de la sección
+    //indica la orientación del corte transversal
 	Vector3D normalIni;
 	
 	//los vectores End asumen que el origen esta centrado en positionIni
+    //y son análogos a los vectores Ini
 	Vector3D positionEnd;		
 	Vector3D normalEnd;
 	
-	
+    //vector de obstáculos de la sección	
 	vector<Obstacle*> obstacles;
+    //vector de esferas azules de la sección
 	vector<BlueSphere*> blueSpheres;
+    //vector de esferas blancas de la sección
 	vector<WhiteSphere*> whiteSpheres;
 	
+    //borrar obstáculos y esferas de la sección
 	void deleteItems();
+    //generar obstáculos y esferas de la sección
 	void createItems();
+    //generar obstáculos y esferas de la sección ignorando los primeros "segmentsLeftOut" segments
 	void createItems( int segmentsLeftOut );
 	
 	public:
@@ -85,10 +98,6 @@ class PathSection {
 			createItems();
 		}
 		
-		// ~PathSection(){cout << "hola??" << endl;
-			// deleteItems();
-		// }
-		
 		//seters
 		void setRadius(float newRadius);
 		void setNumberOfSegments(int newNumberOfSegments);
@@ -111,14 +120,22 @@ class PathSection {
 		Vector3D getNormalEnd();
 		
 		//utils
+        
+        //regenerar los obstáculos y esferas de la sección
 		void resetItems();
+        //regenerar los obstáculos y esferas de la sección ignorando los primeros "segmentsLeftOut" segmentos
 		void resetItems(int segmentsLeftOut);
+        //retorna true si hay un obstáculo en el segmento i
 		bool thereIsObstacle(int i);
+        //retorna true si hay una esfera azúl en el segmento i
 		bool thereIsBlueSphere(int i);
+        //retorna true si hay una esfera blanca en el segmento i
 		bool thereIsWhiteSphere(int i);
+        //retorna el obtáculo del segmento i
 		Obstacle* getObstacle(int i);
+        //retorna la esfera azúl del segmento i
 		BlueSphere* getBlueSphere(int i);
+        //retorna la esfera blanca del segmento i
 		WhiteSphere* getWhiteSphere(int i);
-		Vector3D getPointAtDegree(Vector3D axis, Vector3D orthogonal1, Vector3D orthogonal2, float degree);
 };
 #endif
