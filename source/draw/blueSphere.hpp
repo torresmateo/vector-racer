5c2932e57932a50bcb6eb07ee6b2146951dce9c7
Fer Minardi <Fer Minardi@Fer-notebook>
2013-01-28 09:12:49 -0400

    Combinar

--- source/draw/blueSphere.hpp
+++ source/draw/blueSphere.hpp
@@ -1,10 +1,16 @@
-
+//clase de la Esfera, que cuenta con el efecto de
+//reducir la velocidad en un factor de 1.1 
+//como respuesta a la colisión
 class BlueSphere {
+    //offset horizontal
 	float shift;
+    //radio de la esfera
 	float radius;
+    //bandera de uso del evento de colisión
 	bool triggered;
 	
 	public:
+        //constructores
 		BlueSphere(){
 			triggered = false;
 			radius = 0.035f;
@@ -20,13 +26,18 @@ class BlueSphere {
 			}
 		}
 		
+        //función de dibujado
 		void draw();
+        //detección de colisión
 		bool isCollision();
+        //evento de colisión
 		void trigger();
 		
+        //getters
 		float getShift();
 		float getRadius();
 		
+        //setters
 		void setShift( float newShift );
 		void setRadius( float newRadius );
 };
